3d485dd8f90e8679cd2e807f430d993e0ba1e7bb
Jose Padilla <jose@trackingplan.com>
2025-09-15 09:31:10 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Trackingplan",
-            targets: ["Trackingplan"]),
+            targets: ["Trackingplan"])
 
     ],
     dependencies: [
@@ -24,7 +24,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Trackingplan",
-            dependencies: []),
+            dependencies: ["TrackingplanShared"]),
+        .binaryTarget(
+            name: "TrackingplanShared",
+            path: "./Frameworks/TrackingplanShared.xcframework.zip"),
         .testTarget(
             name: "TrackingplanTests",
             dependencies: ["Trackingplan"]),
